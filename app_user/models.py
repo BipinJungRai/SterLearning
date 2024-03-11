bfaf31b5bc3eb81a9fd61ac6319a506ecde8bdd0
Alex Morris <am04553@surrey.ac.uk>
2024-03-11 21:30:28 +0000

    Added comments in models

--- app_user/models.py
+++ app_user/models.py
@@ -1,3 +1,7 @@
+""" Models to extend the django user model,
+    adding avatars and decorations
+"""
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
