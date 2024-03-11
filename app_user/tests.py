b44fc6e1e58761113e2e4c0c7974010c66d00abd
Alex Morris <am04553@surrey.ac.uk>
2024-03-11 20:14:15 +0000

    Made git ignore media/images and added full_clean to tests

--- app_user/tests.py
+++ app_user/tests.py
@@ -16,6 +16,7 @@ class DecorationTests(TestCase):
                         image=ImageFile(open(ROOT_DIR / image_path, 'rb'),
                                         name=image_path))
         d1.save()
+        d1.full_clean()
     #Test database records creation of new Decoration
     def test_save_decoration(self):
         db_count = Decoration.objects.all().count()
@@ -23,6 +24,7 @@ class DecorationTests(TestCase):
                                 image=ImageFile(open(ROOT_DIR / image_path, 'rb'),
                                         name=image_path))
         decoration.save()
+        decoration.full_clean()
         self.assertEqual(db_count+1, Decoration.objects.all().count())
 
 class UserTests(TestCase):
@@ -32,6 +34,7 @@ class UserTests(TestCase):
                   password='TestPassword1',
                   email='testuser1@test.com')
         u1.save()
+        u1.full_clean()
     #Test that a user can be saved with an avatar
     #and inventory avatar filled
     def test_avatar_user(self):
@@ -42,6 +45,7 @@ class UserTests(TestCase):
                     avatar='DK',
                     inventoryAvatar=['TL', 'DK'])
         user.save()
+        user.full_clean()
         self.assertEqual(db_count+1, User.objects.all().count())
     #Test that a decoration can be saved to
     #a user and added to inventory
@@ -50,6 +54,7 @@ class UserTests(TestCase):
                         image=ImageFile(open(ROOT_DIR / image_path, 'rb'),
                                         name=image_path))
         d1.save()
+        d1.full_clean()
         user = User(username='NewUser',
                     password='NewPassword',
                     email='newuser@test.com',
@@ -57,6 +62,7 @@ class UserTests(TestCase):
                     inventoryAvatar=['TL', 'DK'],
                     decoration=d1)
         user.save()
+        user.full_clean()
         user.inventoryDecoration.add(d1)
         self.assertEqual(user.inventoryDecoration.all().count(),
                          1)
