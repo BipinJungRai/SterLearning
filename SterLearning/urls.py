eb36f2e9b093b08b10a05bf62ad7edef90ad65e6
gp00652 <gp00652@surrey.ac.uk>
2024-04-04 15:21:12 +0100

    added name to 'learn/' view for dtl url

--- SterLearning/urls.py
+++ SterLearning/urls.py
@@ -21,7 +21,7 @@ from app_quiz import views as quiz_views
 # These are the URL patterns specific to the 5 pathways.
 # learn/ is stripped from the start of the URL in the main pattern.
 pathway_patterns =[
-    path("", quiz_views.pathways),
+    path("", quiz_views.pathways, name="pathways-home"),
     path("bank-accounts/", quiz_views.bank),
     path("pensions/", quiz_views.pensions),
     path("taxes/", quiz_views.taxes),
