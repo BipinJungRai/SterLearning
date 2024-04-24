8cb646c10c82f7c5b4f350224bf26b48715acc71
Alex Morris <am04553@surrey.ac.uk>
2024-04-24 20:03:48 +0100

    Added tools and settings page. User can change username via settings page

--- app_user/urls.py
+++ app_user/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import index, AuthGoogle, changeUsername, user_signup, user_login, user_logout
+from .views import index, AuthGoogle, changeUsername, user_signup, user_login, user_logout, user_settings, user_tools
 
 urlpatterns = [
     path("", index, name="index"),
     path("signup", user_signup, name="signup-user"),
     path("auth-receiver", AuthGoogle.as_view(), name='auth-receiver'),
-    path("change-username", changeUsername, name="change_username"),
+    path("change-username", changeUsername, name="change-username"),
     path("login", user_login, name="login"),
-    path("logout", user_logout, name="logout")
+    path("logout", user_logout, name="logout"),
+    path("settings", user_settings, name="settings"),
+    path("tools", user_tools, name="tools"),
 ]
