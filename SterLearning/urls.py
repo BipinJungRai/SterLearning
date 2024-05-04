107b0669c52611b9277afe57b1993845543c02db
Alex Morris <am04553@surrey.ac.uk>
2024-05-05 00:16:08 +0100

    fix urls

--- SterLearning/urls.py
+++ SterLearning/urls.py
@@ -19,11 +19,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from app_quiz import views as quiz_views
-<<<<<<< SterLearning/urls.py
 from app_user import views as user_views
-=======
 from app_tools import views as tools_views
->>>>>>> SterLearning/urls.py
 
 # These are the URL patterns specific to the 5 pathways.
 # learn/ is stripped from the start of the URL in the main pattern.
